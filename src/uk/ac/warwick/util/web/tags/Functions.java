c15d473b4d358a44f7e948f13f91efc9b97b2d1c
Nick Howes <n.howes@warwick.ac.uk>
2007-06-07 10:58:43 +0000

    Now with added more

---
+++ src/uk/ac/warwick/util/web/tags/Functions.java
@@ -0,0 +1,11 @@
+package uk.ac.warwick.util.web.tags;
+
+/**
+ * Collection of generic JSTL functions, 
+ * which don't have any particular dependencies.
+ */
+public class Functions {
+	private Functions() {}
+	
+	
+}
