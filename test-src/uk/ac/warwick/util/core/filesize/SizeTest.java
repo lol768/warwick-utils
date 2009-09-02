d0fbdbdec0f5923a3acc24f8a02b0f58e5feb7bf
Nick Howes <n.howes@warwick.ac.uk>
2009-09-02 14:18:27 +0000

    *** empty log message ***

---
+++ test-src/uk/ac/warwick/util/core/filesize/SizeTest.java
@@ -0,0 +1,21 @@
+package uk.ac.warwick.util.core.filesize;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SizeTest {
+
+	@Test public void workingSize() {
+		Size big = Size.mibibytes(3);
+		assertEquals(3072, big.getKibibytes());
+		assertEquals(3145728, big.getBytes());
+	
+		Size small = Size.kibibytes(128);
+		assertEquals(128, small.getKibibytes());
+		assertEquals(0, small.getMibibytes());
+	}
+
+}
