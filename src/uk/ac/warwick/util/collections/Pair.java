dd4f5f3e3f68e9f12978ecd09e56aff7d77a2b79
Mat Mannion <m.mannion@warwick.ac.uk>
2010-02-19 11:17:19 +0000

    toString for Pair

--- src/uk/ac/warwick/util/collections/Pair.java
+++ src/uk/ac/warwick/util/collections/Pair.java
@@ -19,7 +19,30 @@ public final class Pair<L,R> {
 		return right;
 	}
 	
-	public static<L,R> Pair<L,R>of(L left, R right){
+	public boolean isEqual(Pair<L, R> other) {
+	    return other.getLeft() != null && other.getLeft().equals(left)
+	        && other.getRight() != null && other.getRight().equals(right);
+	}
+	
+	@SuppressWarnings("unchecked")
+    @Override
+    public boolean equals(Object obj) {
+        return obj != null
+            && obj instanceof Pair
+            && isEqual((Pair<L, R>)obj);
+    }
+	
+    @Override
+    public int hashCode() {
+        return toString().hashCode();
+    }
+    
+    @Override
+    public String toString() {
+        return "<" + (left == null ? "null" : left.toString()) + "," + (right == null ? "null" : right.toString()) + ">";
+    }
+    
+    public static<L,R> Pair<L,R>of(L left, R right){
 		return new Pair<L,R>(left, right);
 	}
 }
