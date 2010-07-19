83c694bbf43aae210089b103697db57e75beba22
Nick Howes <n.howes@warwick.ac.uk>
2010-07-19 14:55:20 +0000

    SBTWO-3891 SBTWO-3879

--- src/uk/ac/warwick/util/content/textile2/TransformationOptions.java
+++ src/uk/ac/warwick/util/content/textile2/TransformationOptions.java
@@ -2,6 +2,7 @@ package uk.ac.warwick.util.content.textile2;
 
 public enum TransformationOptions {
 	
-	alwaysUseAlternativeMp3Player
+	alwaysUseAlternativeMp3Player,
+	useNativeAudioVideoElements //HTML5 for audio and video where available
 
 }
