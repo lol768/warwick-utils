a8cbc3f64912b3a51e32c7ed378de08c57f28e66
Nick Howes <n.howes@warwick.ac.uk>
2011-07-04 14:21:59 +0000

    UTL-103

--- src/uk/ac/warwick/util/web/tags/Functions.java
+++ src/uk/ac/warwick/util/web/tags/Functions.java
@@ -1,13 +1,14 @@
 package uk.ac.warwick.util.web.tags;
 
+import java.util.Collection;
+
 import org.apache.commons.lang.StringEscapeUtils;
 
 import uk.ac.warwick.util.core.StringUtils;
 
 
 /**
- * Collection of generic JSTL functions, 
- * which don't have any particular dependencies.
+ * Collection of generic JSTL functions.
  */
 public class Functions {
 	private Functions() {}
@@ -22,5 +23,9 @@ public class Functions {
     
     public static String specialHtml(final String text) {
         return StringUtils.htmlEscapeSpecialCharacters(text);
+    }
+    
+    public static String join(Collection<String> collection, String delimiter) {
+        return StringUtils.join(collection, delimiter);
     }
 }
