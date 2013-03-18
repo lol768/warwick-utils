3bfcdb8908b4e71923109b07fa56e410b447fc32
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2013-03-18 15:23:26 +0000

    TAB-530 - Make interface less lame

--- modules/warwickutils-core/src/main/java/uk/ac/warwick/util/workingdays/WorkingDaysHelper.java
+++ modules/warwickutils-core/src/main/java/uk/ac/warwick/util/workingdays/WorkingDaysHelper.java
@@ -2,6 +2,10 @@ package uk.ac.warwick.util.workingdays;
 
 import org.joda.time.LocalDate;
 
+import java.util.Set;
+
 public interface WorkingDaysHelper {
 	public int getNumWorkingDays(LocalDate start, LocalDate end);
+	public Set<LocalDate> getHolidayDates();
+	public void setHolidayDates(Set<LocalDate> holidayDates);
 }
