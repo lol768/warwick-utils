49519d2f8fa6dc6a86a1ba215ae60313876f4ac3
Mat Mannion <m.mannion@warwick.ac.uk>
2013-07-04 11:02:46 +0100

    Add uk.ac.warwick.util.core.sql package with stub implementations of ResultSet and ResultSetMetaData

---
+++ modules/warwickutils-core/src/main/java/uk/ac/warwick/util/core/sql/AbstractResultSetMetaData.java
@@ -0,0 +1,226 @@
+package uk.ac.warwick.util.core.sql;
+
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+/**
+ * Stub wrapper for {@link java.sql.ResultSetMetaData}
+ * 
+ * @see java.sql.ResultSetMetaData
+ */
+public abstract class AbstractResultSetMetaData implements ResultSetMetaData {
+
+    /**
+     * Returns the number of columns in this <code>ResultSet</code> object.
+     *
+     * @return the number of columns
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public int getColumnCount() throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Indicates whether the designated column is automatically numbered.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public boolean isAutoIncrement(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Indicates whether a column's case matters.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public boolean isCaseSensitive(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Indicates whether the designated column can be used in a where clause.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public boolean isSearchable(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Indicates whether the designated column is a cash value.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public boolean isCurrency(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Indicates the nullability of values in the designated column.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return the nullability status of the given column; one of <code>columnNoNulls</code>,
+     *          <code>columnNullable</code> or <code>columnNullableUnknown</code>
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public int isNullable(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Indicates whether values in the designated column are signed numbers.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public boolean isSigned(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Indicates the designated column's normal maximum width in characters.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return the normal maximum number of characters allowed as the width
+     *          of the designated column
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public int getColumnDisplaySize(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Gets the designated column's suggested title for use in printouts and
+     * displays. The suggested title is usually specified by the SQL <code>AS</code>
+     * clause.  If a SQL <code>AS</code> is not specified, the value returned from
+     * <code>getColumnLabel</code> will be the same as the value returned by the
+     * <code>getColumnName</code> method.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return the suggested column title
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public String getColumnLabel(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Get the designated column's name.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return column name
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public String getColumnName(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Get the designated column's table's schema.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return schema name or "" if not applicable
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public String getSchemaName(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Get the designated column's specified column size.
+     * For numeric data, this is the maximum precision.  For character data, this is the length in characters.
+     * For datetime datatypes, this is the length in characters of the String representation (assuming the
+     * maximum allowed precision of the fractional seconds component). For binary data, this is the length in bytes.  For the ROWID datatype,
+     * this is the length in bytes. 0 is returned for data types where the
+     * column size is not applicable.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return precision
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public int getPrecision(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Gets the designated column's number of digits to right of the decimal point.
+     * 0 is returned for data types where the scale is not applicable.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return scale
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public int getScale(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Gets the designated column's table name.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return table name or "" if not applicable
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public String getTableName(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Gets the designated column's table's catalog name.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return the name of the catalog for the table in which the given column
+     *          appears or "" if not applicable
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public String getCatalogName(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Retrieves the designated column's SQL type.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return SQL type from java.sql.Types
+     * @exception SQLException if a database access error occurs
+     * @see Types
+     */
+    @Override public int getColumnType(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Retrieves the designated column's database-specific type name.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return type name used by the database. If the column type is
+     * a user-defined type, then a fully-qualified type name is returned.
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public String getColumnTypeName(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Indicates whether the designated column is definitely not writable.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public boolean isReadOnly(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Indicates whether it is possible for a write on the designated column to succeed.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public boolean isWritable(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    /**
+     * Indicates whether a write on the designated column will definitely succeed.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     * @exception SQLException if a database access error occurs
+     */
+    @Override public boolean isDefinitelyWritable(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+    //--------------------------JDBC 2.0-----------------------------------
+
+    /**
+     * <p>Returns the fully-qualified name of the Java class whose instances
+     * are manufactured if the method <code>ResultSet.getObject</code>
+     * is called to retrieve a value
+     * from the column.  <code>ResultSet.getObject</code> may return a subclass of the
+     * class returned by this method.
+     *
+     * @param column the first column is 1, the second is 2, ...
+     * @return the fully-qualified name of the class in the Java programming
+     *         language that would be used by the method
+     * <code>ResultSet.getObject</code> to retrieve the value in the specified
+     * column. This is the class name used for custom mapping.
+     * @exception SQLException if a database access error occurs
+     * @since 1.2
+     */
+    @Override public String getColumnClassName(int column) throws SQLException { throw new UnsupportedOperationException(); }
+
+}
