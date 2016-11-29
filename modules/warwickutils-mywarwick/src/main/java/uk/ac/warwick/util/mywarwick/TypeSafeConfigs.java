6b8a96d675eba41bea7dc450ca2c97da162d28ee
kai@shylock <k.lan@warwick.ac.uk>
2016-11-29 10:04:14 +0000

    UTL-167 TypeSafe config implementation for play app

---
+++ modules/warwickutils-mywarwick/src/main/java/uk/ac/warwick/util/mywarwick/TypeSafeConfigs.java
@@ -0,0 +1,61 @@
+package uk.ac.warwick.util.mywarwick;
+
+import uk.ac.warwick.util.mywarwick.model.Config;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Singleton
+public class TypeSafeConfigs implements uk.ac.warwick.util.mywarwick.model.Configs {
+
+    /*
+
+mywarwick.services {
+  config1 {
+    baseUrl=""
+    providerId=""
+    userName=""
+    password=""
+  }
+  config2 {
+    baseUrl=""
+    providerId=""
+    userName=""
+    password=""
+  }
+}
+     */
+
+    @Inject
+    private com.typesafe.config.Config typeSafeConfigProperties;
+
+    List<Config> configList;
+
+    private void initConfigList() {
+        configList = typeSafeConfigProperties
+                .getConfigList("mywarwick.services")
+                .stream()
+                .map(e -> new Config(
+                        e.getString("baseUrl"),
+                        e.getString("providerId"),
+                        e.getString("userName"),
+                        e.getString("password")
+                ))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<uk.ac.warwick.util.mywarwick.model.Config> getConfigs() {
+        if (configList == null) configList = new ArrayList<>();
+        if (configList.size() == 0) initConfigList();
+        return configList;
+    }
+
+    @Override
+    public void setConfigs(List<uk.ac.warwick.util.mywarwick.model.Config> configs) {
+        this.configList = configs;
+    }
+}
