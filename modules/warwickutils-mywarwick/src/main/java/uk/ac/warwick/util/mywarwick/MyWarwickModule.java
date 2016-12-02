38b273d481104f776ef90cfc4207619d7f5ff42f
kai@shylock <k.lan@warwick.ac.uk>
2016-12-02 13:59:37 +0000

    UTL-170 java doc for guice module class and spring config class

--- modules/warwickutils-mywarwick/src/main/java/uk/ac/warwick/util/mywarwick/MyWarwickModule.java
+++ modules/warwickutils-mywarwick/src/main/java/uk/ac/warwick/util/mywarwick/MyWarwickModule.java
@@ -7,7 +7,15 @@ import com.typesafe.config.Config;
 import uk.ac.warwick.util.mywarwick.model.Configuration;
 import uk.ac.warwick.util.mywarwick.model.TypesafeConfiguration;
 
-
+/**
+ * this is a Guice module for Play Application.
+ * to use this module, add the following to conf file:
+ * <pre>
+ * {@code
+ * play.modules.enabled += uk.ac.warwick.util.mywarwick.MyWarwickModule
+ * }
+ * </pre>
+ */
 public class MyWarwickModule extends AbstractModule {
 
     @Override
@@ -17,6 +25,11 @@ public class MyWarwickModule extends AbstractModule {
         bind(HttpClient.class).to(HttpClientImpl.class);
     }
 
+    /**
+     * depended by {@link uk.ac.warwick.util.mywarwick.model.TypesafeConfiguration}
+     * @param playConfig matches if {@link com.typesafe.config.Config} is named with "myWarwickConfig"
+     * @return {@link com.typesafe.config.Config}
+     */
     @Named("myWarwickConfig")
     @Provides
     public Config provideConfigWithUnderlyingPlayConfig(play.api.Configuration playConfig) {
--- modules/warwickutils-mywarwick/src/main/java/uk/ac/warwick/util/mywarwick/MyWarwickSpringConfig.java
+++ modules/warwickutils-mywarwick/src/main/java/uk/ac/warwick/util/mywarwick/MyWarwickSpringConfig.java
@@ -1,19 +1,43 @@
 package uk.ac.warwick.util.mywarwick;
 
 import org.springframework.context.annotation.*;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * this is a configuration bean for Spring (version >= 3) application
+ * use it like this in context XML:
+ * <pre>
+ * {@code
+ * <bean class="uk.ac.warwick.util.mywarwick.MyWarwickSpringConfig" />
+ * }
+ * </pre>
+ *
+ * a properties bean with id "applicationPropertiesForMyWarwick" is expected.
+ * in most cases, there would already have been a properties bean, so alias it like this:
+ * <pre>
+ * {@code
+ * <alias alias="applicationPropertiesForMyWarwick" name="yourPropertiesBeanId" />
+ * }
+ * </pre>
+ */
 @Configuration
 @ComponentScan("uk.ac.warwick.util.mywarwick")
 public class MyWarwickSpringConfig {
 
     @SuppressWarnings("SpringJavaAutowiringInspection")
-    @Inject @Named(value = "applicationPropertiesForMyWarwick")
+    @Inject
+    @Named(value = "applicationPropertiesForMyWarwick")
     Properties propertiesBean;
 
+    /**
+     * this bean is depended by {@link uk.ac.warwick.util.mywarwick.MyWarwickServiceImpl}
+     * @return {@link uk.ac.warwick.util.mywarwick.model.Configuration} which is implemented by {@link uk.ac.warwick.util.mywarwick.model.PropertiesConfiguration}
+     * @throws IOException when properties cannot be loaded from context
+     */
     @Bean
     public uk.ac.warwick.util.mywarwick.model.Configuration configuration() throws IOException {
         return new uk.ac.warwick.util.mywarwick.model.PropertiesConfiguration(propertiesBean);
