065c1b37ae0ee594a54b4a6ef7b9fb3265776f24
kai@shylock <k.lan@warwick.ac.uk>
2017-07-21 14:38:51 +0100

    UTL-185 add an interface

--- modules/warwickutils-mywarwick/src/main/java/uk/ac/warwick/util/mywarwick/model/request/ActivityFactory.java
+++ modules/warwickutils-mywarwick/src/main/java/uk/ac/warwick/util/mywarwick/model/request/ActivityFactory.java
@@ -1,46 +1,16 @@
 package uk.ac.warwick.util.mywarwick.model.request;
 
-import uk.ac.warwick.userlookup.GroupService;
-import uk.ac.warwick.userlookup.UserLookupInterface;
-
-import javax.inject.Inject;
-import javax.inject.Named;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
-@Named(value = "myWarwickActivityFactory")
-public class ActivityFactory {
+public interface ActivityFactory {
 
-    private final UserLookupInterface userLookupInterface;
+    public Activity newActivity();
 
-    private final GroupService groupService;
+    public Activity newActivity(@NotNull String userId, @NotNull String title, String url, String text, @NotNull String type);
 
-    @Inject
-    private ActivityFactory(UserLookupInterface userLookupInterface, GroupService groupService) {
-        super();
-        this.userLookupInterface = userLookupInterface;
-        this.groupService = groupService;
-    }
+    public Activity newActivity(@NotNull Set<String> userIds, @NotNull String title, String url, String text, @NotNull String type);
 
-    public Activity newActivity() {
-        return assignService(new Activity());
-    }
+    public Activity newActivity(@NotNull Set<String> userIds, @NotNull Set<String> groups, @NotNull String title, String url, String text, @NotNull String type);
 
-    public Activity newActivity(@NotNull String userId, @NotNull String title, String url, String text, @NotNull String type) {
-        return assignService(new Activity(userId, title, url, text, type));
-    }
-
-    public Activity newActivity(@NotNull Set<String> userIds, @NotNull String title, String url, String text, @NotNull String type) {
-        return assignService(new Activity(userIds, title, url, text, type));
-    }
-
-    public Activity newActivity(@NotNull Set<String> userIds, @NotNull Set<String> groups, @NotNull String title, String url, String text, @NotNull String type) {
-        return assignService(new Activity(userIds, groups, title, url, text, type));
-    }
-
-    private Activity assignService(Activity activity) {
-        return activity
-                .setGroupService(this.groupService)
-                .setUserLookupInterface(this.userLookupInterface);
-    }
 }
