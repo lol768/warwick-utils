128dc54977c1b5d82258c5cc2395493ace183ca6
kai@shylock <k.lan@warwick.ac.uk>
2017-07-21 11:30:57 +0100

    UTL-185 allow userlookup and groupservice to check recipients

--- modules/warwickutils-mywarwick/src/main/java/uk/ac/warwick/util/mywarwick/model/request/ValidRecipients.java
+++ modules/warwickutils-mywarwick/src/main/java/uk/ac/warwick/util/mywarwick/model/request/ValidRecipients.java
@@ -1,7 +1,12 @@
 package uk.ac.warwick.util.mywarwick.model.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import uk.ac.warwick.userlookup.GroupService;
+import uk.ac.warwick.userlookup.UserLookupInterface;
+import uk.ac.warwick.userlookup.webgroups.GroupNotFoundException;
+import uk.ac.warwick.userlookup.webgroups.GroupServiceException;
 
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 public interface ValidRecipients {
@@ -16,6 +21,30 @@ public interface ValidRecipients {
         Set<String> groups = getGroups();
         boolean isValid = !(users.isEmpty() && groups.isEmpty()) && isValid(users) && isValid(groups);
         return isValid;
+    }
+
+    default boolean isValid(@NotNull UserLookupInterface userLookupInterface) {
+        return isValid() && getUsers().stream().allMatch(e -> isValid(userLookupInterface, e));
+    }
+
+    default boolean isValid(@NotNull GroupService groupService) {
+        return isValid() && getGroups().stream().allMatch(e -> isValid(groupService, e));
+    }
+
+    default boolean isValid(@NotNull UserLookupInterface userLookupInterface, @NotNull GroupService groupService) {
+        return isValid(userLookupInterface) && isValid(groupService);
+    }
+
+    default boolean isValid(@NotNull UserLookupInterface userLookupInterface, String userId) {
+        return userLookupInterface.getUserByWarwickUniId(userId, false).isFoundUser();
+    }
+
+    default boolean isValid(@NotNull GroupService groupService, String groupName) {
+        try {
+            return groupService.getGroupByName(groupName) != null;
+        } catch (GroupNotFoundException | GroupServiceException e) {
+            return false;
+        }
     }
 
     default boolean isValid(String text) {
