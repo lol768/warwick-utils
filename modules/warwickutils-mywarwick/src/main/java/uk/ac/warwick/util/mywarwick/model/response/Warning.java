79304b1d9c692528e17e00d8f99ead9064fe0b82
Kai Lan <k.lan@warwick.ac.uk>
2018-01-08 10:54:24 +0000

    UTL-202 constructors for the new Warning object

--- modules/warwickutils-mywarwick/src/main/java/uk/ac/warwick/util/mywarwick/model/response/Warning.java
+++ modules/warwickutils-mywarwick/src/main/java/uk/ac/warwick/util/mywarwick/model/response/Warning.java
@@ -1,4 +1,13 @@
 package uk.ac.warwick.util.mywarwick.model.response;
 
 public class Warning extends Error {
+
+    public Warning() {
+        super();
+    }
+
+    public Warning(String id, String message) {
+        this.id = id;
+        this.message = message;
+    }
 }
